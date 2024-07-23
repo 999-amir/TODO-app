e83b9a47338f89a1eb3b97a94ceb64e7404f152b
amir mohammad arghavany <amir.arghavoon@gmail.com>
2024-07-23 17:00:06 +0430

    run templates

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class TodoView(View):
+    def get(self, request):
+        context = {
+            'calender_days_name': ['Su', 'Mo', 'Tu', 'We', 'Te', 'Fr', 'Sa'],
+            'calender_days_number': range(1, 32),
+            'calender_months': ['farvardin', 'ordibehesht', 'khordad', 'tir', 'mordad', 'shahrivar', 'mehr', 'aban', 'azar', 'dey', 'bahman', 'esfand'],
+        }
+        return render(request, 'todo/todo.html', context)
