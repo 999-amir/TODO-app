7e8945c9a3d81b3abc68aca613f8eed9d110729d
amir mohammad arghavany <amir.arghavoon@gmail.com>
2024-07-24 17:45:29 +0430

    accounts-DONE

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,9 +14,11 @@ class CostumeUserCreationForm(forms.ModelForm):
 
     def clean(self):
         cd = super().clean()
-        password_1,  password_2 = cd.get('password_1'), cd.get('password_2')
+        password_1, password_2 = cd.get('password_1'), cd.get('password_2')
         if password_1 and password_2 and password_1 != password_2:
             raise ValidationError('!!! password are not the same !!!')
+        elif len(password_1) < 4:
+            raise ValidationError('password should be more than 4 char')
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -32,3 +34,49 @@ class CostumeUserChangeForm(forms.ModelForm):
     class Meta:
         model = CostumeUser
         fields = ('email',)
+
+
+class SignupForm(forms.Form):
+    attrs = {
+        'class': "mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    }
+
+    email = forms.CharField(widget=forms.EmailInput(attrs=attrs))
+    password_1 = forms.CharField(widget=forms.PasswordInput(attrs=attrs))
+    password_2 = forms.CharField(widget=forms.PasswordInput(attrs=attrs))
+
+    def clean(self):
+        cd = super().clean()
+        password_1, password_2 = cd.get('password_1'), cd.get('password_2')
+        if password_1 and password_2:
+            if password_1 != password_2:
+                raise ValidationError('! passwords are not the same !')
+            elif len(password_1) < 4:
+                raise ValidationError('! password should be more than 4 char !')
+        else:
+            raise ValidationError('! password should be added !')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if CostumeUser.objects.filter(email=email).exists():
+            raise ValidationError('! this email has been registered before !')
+        return email
+
+
+class LoginForm(forms.Form):
+    attrs = {
+        'class': "mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    }
+
+    email = forms.CharField(widget=forms.EmailInput(attrs=attrs))
+    password = forms.CharField(widget=forms.PasswordInput(attrs=attrs))
+
+
+class ProfileForm(forms.Form):
+    f_name = forms.CharField(widget=forms.TextInput)
+    l_name = forms.CharField(widget=forms.TextInput)
+    description = forms.CharField(widget=forms.TextInput)
+
+
+class ProfileDescriptionForm(forms.Form):
+    description = forms.CharField(widget=forms.TextInput)
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
-    path('login/', views.LoginView.as_view(), name='login')
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile-description', views.ProfileDescription.as_view(), name='profile_description')
 ]
